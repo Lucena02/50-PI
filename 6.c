4ffea75ef7d66035ab799469f79ed2dbe2a460ee
Lucena02 <riku.lucena@hotmail.com>
2021-05-18 17:15:26 +0100

    Eliminação de ficheiros desnecessários

--- 6.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int qDig (unsigned int n) {
-    int c = 0;
-    if (n == 0) return 1;
-    for (; n > 0; n = n/10) c++;
-    return c;
-}
--- 8.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-char *mystrcpy (char *dest, char source[]) {
-    int i;
-    for(i=0;source[i] != '\0'; i++) {
-        dest[i] = source[i];
-    }
-    dest[i] = '\0';
-}
